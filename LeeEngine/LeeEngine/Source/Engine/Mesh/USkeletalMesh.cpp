a97df1674a0fc8dff851944d25793f2e4a0bba94
Lee Yoon Seok <kjggr123@gmail.com>
2025-03-24 16:59:03 +0900

    기존 프로젝트에서 사용했던 스켈레탈 메시를 단순하게 렌더링해보는것을 목표로 클래스 구현 진행중

---
+++ LeeEngine/LeeEngine/Source/Engine/Mesh/USkeletalMesh.cpp
@@ -0,0 +1,1 @@
+#include "USkeletalMesh.h"
---
+++ LeeEngine/LeeEngine/Source/Engine/Mesh/USkeletalMesh.h
@@ -0,0 +1,112 @@
+// 03.24
+// 언리얼 엔진 5 코드를 분석하며 자체엔진으로 작성중인 코드입니다.
+// 언리얼엔진의 코딩컨벤션을 따릅니다.  https://dev.epicgames.com/documentation/ko-kr/unreal-engine/coding-standard?application_version=4.27
+// 이윤석
+
+#pragma once
+#include "Engine/MyEngineUtils.h"
+#include "Engine/AssetManager/AssetManager.h"
+#include "Engine/UObject/UObject.h"
+
+
+class FTest
+{
+public:
+	FTest(const nlohmann::json& StaticMeshFilePathData)
+	{
+		for(auto& p : VertexBuffer)
+		{
+			p->Release();
+		}
+		for(auto& p : IndexBuffer)
+		{
+			p->Release();
+		}
+		for(auto& p : TextureSRV)
+		{
+			p->Release();
+		}
+
+		AssetManager::LoadModelData(StaticMeshFilePathData["ModelData"],GDirectXDevice->GetDevice(),VertexBuffer,IndexBuffer);
+		MeshCount = VertexBuffer.size();
+		int TextureArraySize = StaticMeshFilePathData["TextureData"].size();
+		for(int count = 0; count < MeshCount; ++count)
+		{
+			if(TextureArraySize < count)
+			{
+				/*MY_LOG(StaticMeshFilePathData["Name"], EDebugLogLevel::DLL_Warning, "Texture count not match with mesh count! ");*/
+				break;
+			}
+
+			Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> SRV;
+			const std::string TextureFilePath = StaticMeshFilePathData["TextureData"][count];
+			AssetManager::LoadTextureFromFile(std::wstring().assign(TextureFilePath.begin(),TextureFilePath.end()), GDirectXDevice->GetDevice(), SRV);
+			TextureSRV.push_back(SRV);
+		}
+
+		if(TextureSRV.size() == 0)
+		{
+			/*MY_LOG(StaticMeshFilePathData["Name"], EDebugLogLevel::DLL_Error, "In static mesh .myasset file, texture data no valid");*/
+			return;
+		}
+
+		int currentTextureCount = TextureSRV.size();
+		for(; currentTextureCount < MeshCount; ++currentTextureCount)
+		{
+			TextureSRV.push_back(TextureSRV[0]);
+		}
+	}
+
+
+protected:
+private:
+public:
+	unsigned int MeshCount;
+
+	// Buffer[MeshCount]
+	std::vector<Microsoft::WRL::ComPtr<ID3D11Buffer>> VertexBuffer;
+	std::vector<Microsoft::WRL::ComPtr<ID3D11Buffer>> IndexBuffer;
+
+	// TODO: 임시 텍스쳐 데이터,
+	// UMaterial 구현 이전으로 텍스쳐 SRV 데이터를 해당 클래스에서 관리
+	std::vector<Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>> TextureSRV;
+};
+
+
+class USkeletalMesh : public UObject, public std::enable_shared_from_this<USkeletalMesh>
+{
+	MY_GENERATED_BODY(USkeletalMesh)
+public:
+	USkeletalMesh();
+	~USkeletalMesh();
+
+	unsigned int GetSkeletalMeshMeshCount() const {return RenderData.get()->MeshCount; }
+
+	// unique_ptr 로 관리되는 RenderData
+	// Render 시 잠깐 사용하므로 로우 포인터를 반환
+	const FTest* GetStaticMeshRenderData() const {return RenderData.get();}
+
+	static const std::map<std::string, std::shared_ptr<USkeletalMesh>>& GetStaticMeshCache() {return GetStaticMeshCacheMap();}
+	static const std::shared_ptr<USkeletalMesh>& GetStaticMesh(const std::string& StaticMeshName) { return GetStaticMeshCacheMap()[StaticMeshName]; }
+
+
+	virtual void LoadDataFromFileData(const nlohmann::json& AssetData) override;
+protected:
+private:
+
+public:
+protected:
+private:
+	static std::map<std::string, std::shared_ptr<USkeletalMesh>>& GetStaticMeshCacheMap()
+	{
+		static std::map<std::string, std::shared_ptr<USkeletalMesh>> SkeletalMeshCache;
+		return SkeletalMeshCache;
+	}
+
+
+
+	// TODO: LOD 데이터가 필요한 경우 std::map<UINT, std::unique_ptr<FStaticMeshRenderData> LODRenderData; 로 변경 예정
+	// StaticMesh의 버텍스, 인덱스 버퍼 등 렌더링에 필요한 데이터를 관리
+	std::unique_ptr<FTest> RenderData;
+
+};
