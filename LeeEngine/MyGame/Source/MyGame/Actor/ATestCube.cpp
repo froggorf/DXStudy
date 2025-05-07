fc3badba0054a43d21f16637c382e96d1df3d8cd
Lee Yoon Seok <kjggr123@gmail.com>
2025-05-07 17:23:24 +0900

    리본 렌더러를 구현하기 위한 정리

--- LeeEngine/MyGame/Source/MyGame/Actor/ATestCube.cpp
+++ LeeEngine/MyGame/Source/MyGame/Actor/ATestCube.cpp
@@ -29,7 +29,7 @@ ATestCube::ATestCube()
 
 	NiagaraComp = std::make_shared<UNiagaraComponent>();
 	NiagaraComp->SetupAttachment(GetRootComponent());
-	std::shared_ptr<UNiagaraSystem> System = UNiagaraSystem::GetNiagaraAsset("NS_Mesh");
+	std::shared_ptr<UNiagaraSystem> System = UNiagaraSystem::GetNiagaraAsset("NS_Test");
 	NiagaraComp->SetNiagaraAsset(System);
 	NiagaraComp->SetRelativeLocation(XMFLOAT3{-45.0f,-15.0f,0.0f});
 
